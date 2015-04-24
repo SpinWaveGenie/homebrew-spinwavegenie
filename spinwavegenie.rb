368acba0a615a69cdd4e4d24c6d3c6013798f445
Steven Hahn <hahnse@ornl.gov>
2015-04-24 18:27:33 -0400

    update sha256 for  v0.1.0 download

--- spinwavegenie.rb
+++ spinwavegenie.rb
@@ -3,6 +3,7 @@ require "formula"
 class Spinwavegenie < Formula
   homepage "https://github.com/SpinWaveGenie/SpinWaveGenie"
   url "https://github.com/SpinWaveGenie/SpinWaveGenie/archive/v0.1.0.tar.gz"
+  sha256 "ab7ee3ac3d86640cc0abc2e2aa0c2d8973337c3f5f957f5b14f9f9b5b94d6956"
   head "https://github.com/SpinWaveGenie/SpinWaveGenie.git", :branch => "master"
 
   depends_on "cmake" => :build
